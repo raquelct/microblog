0ecb44f9b279fd23881fcfec4473e3223b7a510b
Caio Teixeira <caiofct@ifce.edu.br>
2016-12-05 14:02:41 -0300

    Resolvido bug com imagens não sendo exibidos no heroku

--- config/application.rb
+++ config/application.rb
@@ -8,8 +8,13 @@ Bundler.require(*Rails.groups)
 
 module Microblog
   class Application < Rails::Application
-    # Settings in config/environments/* take precedence over those specified here.
-    # Application configuration should go into files in config/initializers
-    # -- all .rb files in that directory are automatically loaded.
+    config.encoding = 'utf-8'
+    config.time_zone = 'America/Fortaleza'
+    config.i18n.default_locale = :'en'
+    # config.i18n.available_locales = %w(pt-BR en)
+    # config.active_record.raise_in_transactional_callbacks = true
+    config.assets.paths << Rails.root.join('app', 'assets', 'fonts')
+
+    config.assets.precompile += %w( .svg .otf .eot .woff .ttf)
   end
 end
