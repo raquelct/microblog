21208faeaf3e6b2b8eb5ff393250172a458ba9b6
raquel <raquelct97@gmail.com>
2016-12-05 14:07:53 -0300

    Ajuste do mailer

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -88,11 +88,11 @@ Rails.application.configure do
 
   ActionMailer::Base.delivery_method = :smtp
   ActionMailer::Base.smtp_settings = {
-    address:        "smtp.gmail.com",
+    address:        "smtp.sendgrid.net",
     port:           "587",
     authentication: :plain,
-    user_name:     ENV['GMAIL_USERNAME'],
-    password:      ENV['GMAIL_PASSWORD'],
-    enable_starttls_auto:  true
+    user_name:     ENV['SENDGRID_USERNAME'],
+    password:      ENV['SENDGRID_PASSWORD'],
+    domain:        "heroku.com"
   }
 end
